1e08a8630e014bbfb4f71a4b5e5f2014d97eeb2d
ddfilipov <denisfilipov12+3@gmail.com>
2023-04-10 21:20:54 +0200

    installed styled-components

--- src/components/SeatRow.tsx
+++ src/components/SeatRow.tsx
@@ -1,20 +1,26 @@
 import { FC } from "react";
 import { seats } from "../data/plane-seats";
 import { Row, SegmentSeatMap } from "../types/IPlaneSeats";
+import styled from "styled-components";
 
 interface SeatRowProps {
     row: Row;
     rowNumber: number;
 }
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
 const SeatRow: FC<SeatRowProps> = ({ row, rowNumber }) => {
     return (
-        <div>
+        <Container>
             {rowNumber}:
             {row.seats.map((seat) => (
                 <div>{seat.seatLetter}</div>
             ))}
-        </div>
+        </Container>
     );
 };
 
