4a7be2c7fa5510ed658c6d75c68b3e075964a71c
denis.filipov <denis.filipov@patterson.agency>
2023-04-11 08:58:44 +0200

    empty seats have a different color

--- src/components/atoms/Seat.tsx
+++ src/components/atoms/Seat.tsx
@@ -6,20 +6,33 @@ import { ISeat } from "../../types/IPlaneSeats";
 
 interface SeatProps extends ISeat {}
 
-const Container = styled.div`
+interface IAvailableSeat {
+    isSeatAvailable: boolean;
+}
+
+const SeatWrapper = styled.div<IAvailableSeat>`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    background-color: #f6cbfc;
+    background-color: ${(props) => (props.isSeatAvailable ? "#f6cbfc" : "#E6E6E6")};
     border-radius: 10px;
     width: 40px;
     height: 40px;
     color: #14544e;
 `;
+const AilseWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+`;
 
 const Seat: FC<SeatProps> = ({ characteristics, price, seatKey: key, seatLetter, ailse }) => {
-    return price ? <Container>{`${price?.amount}€`}</Container> : null;
+    const seatPrice = price ? price.amount.toString() : "X";
+    return <SeatWrapper isSeatAvailable={price ? true : false}>{`${seatPrice}`}</SeatWrapper>;
 };
 
 export default Seat;
